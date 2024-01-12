678c53712ddf5046ee4c62417389112b33f09eaa
Obar <ducwebdev@gmail.com>
2024-01-12 10:24:06 +0700

    save

--- client/src/util/url.js
+++ client/src/util/url.js
@@ -1,7 +1,7 @@
 const url = {
   // root: 'https://boutique2-2v5d.onrender.com'
   // root: window.location.href
-  root: 'http://localhost:5000/api'
+  root: 'https://ecommerce-hnh0.onrender.com/api'
 }
 
 export default url
